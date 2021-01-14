247a9d819c4f5a0a03cd4989c10a5c6e70cdd0ec
Alexandr <75570159+Alexenterprises@users.noreply.github.com>
2021-01-14 23:11:13 +0200

    capitalCase.js
    v.1.0.

---
+++ capitalCase.js
@@ -0,0 +1,15 @@
+function toCapitaCase() {
+  let string = 'lorem ipsum stan';
+  var splits = string.split(" ");
+  var update = "";
+
+  for (let i = 0; i < splits.length; i++) {
+    let Name = splits[i];
+    let First = Name.substring(0, 1).toUpperCase();
+    let Leftovers = Name.substring(1, Name.length)
+    update += First + Leftovers + " ";
+  }
+
+  console.log(update)
+}
+toCapitaCase();
